cbdd435e0f6cc987d9b043ec9a6c185e761e951a
akhileshrana123455 <60541736+akhileshrana123455@users.noreply.github.com>
2021-09-01 18:25:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "-> 3.0"
+        }
+    }
+}
 provider "aws" {
     access_key = "AKIAZJLI7HSFPUQFVX65"
     secret_key = "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"
